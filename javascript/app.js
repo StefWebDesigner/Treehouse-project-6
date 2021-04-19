efb6e46e535694ef20cb6d5c49c33d52c4c9f35c
StefWebDesigner <67237726+StefWebDesigner@users.noreply.github.com>
2021-04-19 15:43:04 -0400

    starting from fresh

---
+++ javascript/app.js
@@ -0,0 +1,77 @@
+//My EFFORTS TO SIMPLY THIS DOWN
+
+
+
+//=======Starting Variables=========
+//START GAME VARIABLES
+const phrases = [ 
+ 'what did the array say to the other array', 
+    'what an array of sunshine today', 
+    'what are arrays favorite animal', 
+    'a ray of course',
+    'what are rays coding specailty',
+    'arrays dah'
+	];
+
+
+// FUNCTION
+
+//RANDOM PHRASE FUNCTION
+
+
+// DISPLAYING THE PHRASE FUNCTION
+
+
+// STARTS THE GAME
+
+     
+
+    // RESET THE GAME
+        // RESETS PHRASE TO GUESS
+        //CONNECT WITH PHRASE SECTION
+   
+        //RESORTING 5 LIVES
+        //TARGET TRIES
+        
+        //TARGET THE HEART IMAGE
+      
+        // RESTART MISSED ATTEMPTS SCOREBOARD
+
+        // SETS PHRASE TO GUESS
+  
+         //Test
+
+
+//CHECKS THE LETTER 
+
+    //CREATE A SELECTER LETTER VARIABLE & TARGET IT
+
+
+    //CONNECT SELECTED LETTERS TO LETTERS MATCHED/FOUND
+  
+ 
+    //CALL LETTERS FOUND
+
+
+
+// CHECKING SYSTEM FOR WIN/LOST
+
+    //INCLUDE SOMETHING WITH MESSAGES AND STUFF
+    //WIN MESSAGE
+    
+     
+    //LOSE MESSAGE
+    
+    
+
+// GAMEPLAY EVENT STARTER
+
+//TARGET THE EVENT
+    
+
+    //MATCH SELECTED WIHT BUTTION & SOME DISABLE FEATURE
+
+
+
+
+//=======================================================
